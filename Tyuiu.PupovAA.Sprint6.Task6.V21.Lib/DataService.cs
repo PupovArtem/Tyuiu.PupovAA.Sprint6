5d0373a696b486718c1cd95c0596567b79984e33
Artem Pupov <Tyuiu.PupovAA@outlook.com>
2025-12-23 22:57:14 +0500

    Task6

---
+++ Tyuiu.PupovAA.Sprint6.Task6.V21.Lib/DataService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.PupovAA.Sprint6.Task6.V21.Lib
+{
+    public class DataService : ISprint6Task6V21
+    {
+        public string CollectTextFromFile(string path)
+        {
+            if (!File.Exists(path))
+            {
+                throw new FileNotFoundException("Файл не найден", path);
+            }
+
+            string text = File.ReadAllText(path);
+
+            
+            char[] separators =
+            {
+                ' ', '\t', '\r', '\n',
+                ',', '.', ';', ':', '!', '?',
+                '-', '—', '(', ')', '[', ']', '\"', '\'', '/'
+            };
+
+            string[] parts = text.Split(
+                separators,
+                StringSplitOptions.RemoveEmptyEntries);
+
+            List<string> resultWords = new List<string>();
+
+            foreach (string word in parts)
+            {
+
+                if (word.Contains('g') || word.Contains('G'))
+                {
+                    resultWords.Add(word);
+                }
+            }
+
+            
+            return string.Join(" ", resultWords);
+        }
+    }
+}
+ 
---
+++ Tyuiu.PupovAA.Sprint6.Task6.V21.Test/DataServiceTest.cs
@@ -0,0 +1,30 @@
+﻿
+using System.IO;
+using Tyuiu.PupovAA.Sprint6.Task6.V21.Lib;
+
+namespace Tyuiu.PupovAA.Sprint6.Task6.V21.Test
+{
+    [TestClass]
+    public class Test1
+    {
+        [TestMethod]
+        public void CheckCollectTextFromFile()
+        {
+            
+            string dir = Path.GetTempPath();
+            string path = Path.Combine(dir, "Текстовый документ");
+            File.WriteAllText(path, "good day great GAME test");
+
+            DataService ds = new DataService();
+
+            
+            string actual = ds.CollectTextFromFile(path);
+
+            
+            string expected = "good great GAME";
+
+            
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
