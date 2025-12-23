1018f96a584fe222091dde586e2cb13b1f7cd212
Artem Pupov <Tyuiu.PupovAA@outlook.com>
2025-12-23 21:37:32 +0500

    Task5

--- Tyuiu.PupovAA.Sprint6.Task4.V29.Test/DataServiceTest.cs
+++ Tyuiu.PupovAA.Sprint6.Task4.V29.Test/DataServiceTest.cs
@@ -7,6 +7,15 @@ namespace Tyuiu.PupovAA.Sprint6.Task4.V29.Test
         [TestMethod]
         public void TestMethod1()
         {
+            DataService ds = new DataService();
+            int startValue = -5;
+            int stopValue = 5;
+            double[] res = ds.GetMassFunction(startValue, stopValue);
+            double[] wait = { -32.26, -27.5, -22.8, -17.95, -12.97, -9.0, -0.31, 3.77, 8.57, 13.42, 18.28 };
+            for (int i = 0; i < res.Length; i++)
+            {
+                Assert.AreEqual(res[i], wait[i]);
+            }
         }
     }
 }
--- Tyuiu.PupovAA.Sprint6.Task5.V23.Test/DataServiceTest (1).cs
+++ Tyuiu.PupovAA.Sprint6.Task5.V23.Test/DataServiceTest (1).cs
@@ -8,6 +8,11 @@ namespace Tyuiu.PupovAA.Sprint6.Task5.V23.Test
         [TestMethod]
         public void TestMethod1()
         {
+            string path = @"C:\Users\katag\source\repos\Tyuiu.PupovAA.Sprint6\Tyuiu.PupovAA.Sprint6.Task5.V23";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
         }
     }
 }
